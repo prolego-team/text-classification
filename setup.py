77f7d376937ecb1a9d2d67fd5bc64f24a1438d81
arti <artimpatel00@gmail.com>
2022-08-16 13:17:55 -0400

    check >=3.6 compatibility

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ),
     description="Use transformers to classify text.",
     long_description=open('README.md').read(),
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     install_requires=[
         "click>=8.0.1",
         "numpy>=1.21.1",
